a2ae9938bc4c3bd0088279d297b004599bac1c7e
macbookpro <1>
2019-11-18 15:36:46 -0300

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+## [Versão] - Data de alteração
+
+* Explicações sobre as alterações
+
+
+## [0.0.1] - 18/11/2019.
+
+*  Algo foi atualizado
---
+++ lib/tutorial_package_example.dart
@@ -0,0 +1,36 @@
+library tutorial_package_example;
+
+import 'package:flutter/material.dart';
+
+class TutorialPackageExample {
+  static Future showCustomAlertBox({
+    @required BuildContext context,
+    @required Widget willDisplayWidget,
+  }) {
+    assert(context != null, "context is null!!");
+    assert(willDisplayWidget != null, "willDisplayWidget is null!!");
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                willDisplayWidget,
+                MaterialButton(
+                  color: Colors.white30,
+                  child: Text('close alert'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+            elevation: 10,
+          );
+        });
+  }
+}
